48799dacbaf7549642c23ffbae766392a957a99d
Pradeep Kumar <pradeepkr47b@gmail.com>
2019-05-25 13:58:22 +0530

    alerts page added

--- funtoot/models.py
+++ funtoot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Buttons(models.Model):
+    stitched_start = models.BooleanField(default=True)
+    stitched_stop = models.BooleanField(default=True)
+    time_series_start = models.BooleanField(default=True)
+    time_series_stop = models.BooleanField(default=True)
--- funtoot/views.py
+++ funtoot/views.py
@@ -5,24 +5,37 @@ import os
 import signal
 import subprocess
 
-pro = 123
+from funtoot.models import Buttons
+import pandas as pd
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def schedule(request):
-    return render(request, 'schedule.html')
+    row = Buttons.objects.get(id=1)
+    data = {
+        'stitched_start': row.stitched_start,
+        'stitched_stop': row.stitched_stop,
+        'time_series_start': row.time_series_start,
+        'time_series_stop': row.time_series_stop
+    }
+    return render(request, 'schedule.html', data)
 
 def alert(request):
-    return render(request, 'alerts.html')
+    df = pd.read_csv('all_alerts.csv').to_dict('r')
+    data = {'data': df}
+    return render(request, 'alert_page.html', data)
 
 def stitched_start(request):
-    # os.system('python schedule_tasks')
-    cmd = 'python schedule_tasks.py' 
-    # The os.setsid() is passed in the argument preexec_fn so
-    # it's run after the fork() and before  exec() to run the shell.
-    pro = subprocess.Popen(cmd, stdout=subprocess.PIPE, 
-                       shell=True, preexec_fn=os.setsid)
-    return HttpResponse("Hello, world. You're at the polls index.")
-    print("killing the process")
-    os.killpg(os.getpgid(pro.pid), signal.SIGTERM)  # Send the signal to all the process groups
+    pass
+    # # os.system('python schedule_tasks')
+    # cmd = 'python schedule_tasks.py' 
+    # # The os.setsid() is passed in the argument preexec_fn so
+    # # it's run after the fork() and before  exec() to run the shell.
+    # pro = subprocess.Popen(cmd, stdout=subprocess.PIPE, 
+    #                    shell=True, preexec_fn=os.setsid)
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    # print("killing the process")
+    # os.killpg(os.getpgid(pro.pid), signal.SIGTERM)  # Send the signal to all the process groups
 
